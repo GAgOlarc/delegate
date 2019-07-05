fb4594313632ebf1e293a6035ba6276515b0d013
Your Name <you@example.com>
2019-07-05 07:16:56 +0100

    Add project files.

---
+++ Delegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        delegate void SayGreeting(string name);
+
+        public static void SayGoodby(string name)
+        {
+            Console.WriteLine(string.Format("Goodby {0}", name));
+        }
+        static void Main(string[] args)
+        {
+            SayGreeting sayGreeting = delegate (string name)
+            {
+                Console.WriteLine(string.Format("Hello {0}", name));
+            };
+
+            Console.WriteLine("What's your name?");
+            string input = Console.ReadLine();
+            sayGreeting(input);
+            sayGreeting = new SayGreeting(SayGoodby);
+            sayGreeting(input);
+        }
+    }
+}
